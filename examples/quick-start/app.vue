1a4fddb0a37ddce2fa8ee281c83eece3e7c259b7
Gurvan <gurvan.freelance@gmail.com>
2022-12-29 19:36:04 +0100

    fix: build path & 404 error in production

--- examples/quick-start/app.vue
+++ examples/quick-start/app.vue
@@ -1,14 +1,5 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <!-- Edit this file to play around with Nuxt but never commit changes! -->
   <div>
     <NuxtWelcome />
   </div>
 </template>
-
-<style scoped>
-
-</style>
