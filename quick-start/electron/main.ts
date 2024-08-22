eb184944126caa4cd2be51c1ff6c2bf8e4401185
Yue JIN <yjin@nustarnuclear.com>
2024-08-22 13:26:03 +0800

    fix: load fixed url in dev

--- quick-start/electron/main.ts
+++ quick-start/electron/main.ts
@@ -1,38 +1,21 @@
 import { app, BrowserWindow, ipcMain } from 'electron'
 import path from 'node:path'
 
-// The built directory structure
-//
-// ├─┬ dist-electron
-// │ ├─┬ main
-// │ │ └── index.js
-// │ ├─┬ preload
-// │ │ └── index.js
-// │ ├─┬ renderer
-// │ │ └── index.html
-process.env.APP_ROOT = path.join(__dirname, '..')
-
-export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
-export const RENDERER_DIST = path.join(process.env.APP_ROOT, '.output/public')
-
-process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
-  ? path.join(process.env.APP_ROOT, 'public')
-  : RENDERER_DIST
 
 let win: BrowserWindow | null
 
 function createWindow() {
   win = new BrowserWindow({
     webPreferences: {
-      preload: path.join(MAIN_DIST, 'preload.js'),
+      preload: path.join(__dirname, 'preload.js'),
     },
   })
 
-  if (process.env.VITE_DEV_SERVER_URL) {
-    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL(JSON.parse(process.env.__NUXT_DEV__!).proxy.url)
     win.webContents.openDevTools()
   } else {
-    win.loadFile(path.join(process.env.VITE_PUBLIC!, 'index.html'))
+    win.loadFile('.output/public/200.html')
   }
 }
 
